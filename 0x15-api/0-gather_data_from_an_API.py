e2aabb238da38f673b519815d0555ffb3659e13b
IOLevi <337@holbertonschool.com>
2018-08-28 00:31:07 +0000

    pep8 fixes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,7 +5,8 @@ import sys
 if __name__ == '__main__':
     employee_id = sys.argv[1]
     tasksr = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id))
+        "https://jsonplaceholder.typicode.com/todos?userId={}".
+        format(employee_id))
     r = tasksr.json()  # list of dictionaries
     number_of_tasks = len(r)
     tasks_completed = 0
@@ -17,8 +18,9 @@ if __name__ == '__main__':
         "https://jsonplaceholder.typicode.com/users/{}".format(employee_id))
     employee_name = response.json().get('name', None)
 
-    print("Employee {} is done with tasks({}/{}):".format(employee_name,
-                                                          tasks_completed, number_of_tasks))
+    print("Employee {} is done with tasks({}/{}):".
+          format(employee_name,
+                 tasks_completed, number_of_tasks))
 
     for task in r:
         if task.get('completed', None):
